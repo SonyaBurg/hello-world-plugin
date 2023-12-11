75df56ae2798945c1a1262c1377221981c6f72a6
GitHub Action <action@github.com>
2023-12-11 23:03:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/sonyaburg/helloworldplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/sonyaburg/helloworldplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.sonyaburg.helloworldplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sonyaburg.helloworldplugin.MyBundle
+import com.github.sonyaburg.helloworldplugin.services.MyProjectService
 import javax.swing.JButton
 
 
